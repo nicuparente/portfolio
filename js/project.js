006d7f62286ff86d7a3349601364b30e89edbb2c
Nicu Parente <nicuparente@gmail.com>
2017-05-09 22:18:11 -0700

    Rough Draft

---
+++ js/project.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function Project(rawProject){
+  this.title = rawProject.title;
+  this.date = rawProject.date;
+  this.url = rawProject.url;
+  this.description = rawProject.description;
+}
